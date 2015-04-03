201280b0c0355e28f0d34f3ca7e6496b2161191b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-04-03 21:14:37 +0200

    Created abstract implementation for controllers

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -14,4 +14,10 @@ abstract class AbstractController
     {
         $this->app = $app;
     }
+
+    protected function getRouteParam($name, $default = null)
+    {
+        $routeParams = $this->app->router()->getCurrentRoute()->getParams();
+        return array_key_exists($name, $routeParams) && ! empty($routeParams[$name]) ? $routeParams[$name] : $default;
+    }
 }
--- src/Controller/AbstractRestfulController.php
+++ src/Controller/AbstractRestfulController.php
@@ -3,35 +3,172 @@ namespace Acelaya\Controller;
 
 abstract class AbstractRestfulController extends AbstractController
 {
+    const IDENTIFIER_KEY = 'id';
+
+    /**
+     * @var array
+     */
+    protected $queryParams = [];
+
     final public function dispatchAction()
     {
+        $resp = [];
+        $this->queryParams = $this->app->request()->get();
+        $id = $this->getRouteParam(self::IDENTIFIER_KEY);
 
+        // Dispatch based on HTTP verb
+        switch (strtolower($this->app->request()->getMethod())) {
+            case 'get':
+                if ($this->isJsonpRequest()) {
+                    $resp = $this->processJsonpRequest($this->queryParams['action'], $id);
+                } else {
+                    $resp = isset($id) ? $this->get($id) : $this->getList();
+                }
+                break;
+            case 'post':
+                $resp = $this->create($this->parseRequestBody());
+                break;
+            case 'put':
+                $resp = $this->update($id, $this->parseRequestBody());
+                break;
+            case 'delete':
+                $resp = $this->delete($id);
+                break;
+            default:
+                $this->app->response()->setStatus(405);
+        }
+
+        // Write response and content-type
+        $responseContent = json_encode($resp);
+        if ($this->isJsonpRequest()) {
+            $responseContent = sprintf('%s(%s)', $this->queryParams['callback'], $responseContent);
+            $this->app->response()->header('Content-type', 'application/javascript');
+        } else {
+            $this->app->response()->header('Content-type', 'application/json');
+        }
+        $this->app->response()->write($responseContent);
+
+    }
+
+    protected function isJsonpRequest()
+    {
+        return
+            strtolower($this->app->request()->getMethod()) === 'get'
+            && isset($this->queryParams['callback'])
+            && isset($this->queryParams['action']);
     }
 
     public function get($id)
     {
-        
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
     }
 
     public function getList()
     {
-
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
     }
 
     public function create(array $data)
     {
-
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
     }
 
     public function update($id, array $data)
     {
-
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
     }
 
     public function delete($id)
     {
-
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
     }
 
     // So on...
+
+    public function head($id = null)
+    {
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
+    }
+
+    public function options()
+    {
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
+    }
+
+    public function patch($id, $data)
+    {
+        $this->app->response()->setStatus(405);
+        return [
+            'error' => 'Method Not Allowed'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function parseRequestBody()
+    {
+        // Try to decode from JSON
+        $content = $this->app->request()->getBody();
+        $parsedArgs = json_decode($content, true);
+        if (is_array($parsedArgs)) {
+            return $parsedArgs;
+        }
+
+        // Try to parse from standard params string
+        $parsedArgs = [];
+        parse_str($content, $parsedArgs);
+        return $parsedArgs;
+    }
+
+    /**
+     * @param $action
+     * @param null $id
+     * @return array
+     */
+    protected function processJsonpRequest($action, $id = null)
+    {
+        $resp = [];
+
+        // Dispatch based on action param
+        switch ($action) {
+            case 'read':
+                $resp = isset($id) ? $this->get($id) : $this->getList();
+                break;
+            case 'create':
+                $resp = $this->create($this->parseRequestBody());
+                break;
+            case 'update':
+                $resp = $this->update($id, $this->parseRequestBody());
+                break;
+            case 'delete':
+                $resp = $this->delete($id);
+                break;
+            default:
+                $this->app->response()->setStatus(405);
+        }
+
+        return $resp;
+    }
 }
